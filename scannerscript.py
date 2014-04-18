893a219e2ba57a9ab90b39c277cd8852a260c1dc
Joost Rijneveld <joostrijneveld@gmail.com>
2014-04-18 23:04:19 +0200

    Grab filename from traffic, robustness against random nullbytes and very small files

--- scannerscript.py
+++ scannerscript.py
@@ -1,5 +1,4 @@
-import socket, sys
-import os.path
+import socket, sys, os
 
 HOST = ''    # Symbolic name meaning the local host
 PORT = 37100 # Arbitrary non-privileged port dictated by the scanner daemon
@@ -20,65 +19,108 @@ print 'Socket bind complete'
 
 # program loop
 while True:
-    s.listen(1)
-    print 'Socket now listening'
-    # Accept the connection once
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    conn.send('\x4F\x4B\x00\x05') # initial 'OK05'
-    print 'sent 1'
-    data = conn.recv(8) # should be \x00\x06\x34\x00\x00\x02\x00\x00
-    print 'recv 1',data.encode('hex')
-    conn.send('\x00\x6a\x34\x10\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
-    print 'sent 2'
-    data = conn.recv(542) # seems to be 542
-    print 'recv 2', data.encode('hex')
-    conn.send('\x00\x0a\x34\x11\x00\x01\x00\x00\xff\xff\xff\xff')
-    print 'sent 3'
-    data = conn.recv(1024) # 6 bytes? or 86 bytes?
-    print 'recv 3', data.encode('hex')
+    try:
+        s.listen(1)
+        gotnullbytes = False # track if we have received the nullbytes yet
+        print 'Socket now listening'
+        # Accept the connection once
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        conn.send('\x4F\x4B\x00\x05') # initial 'OK05'
+        data = conn.recv(1024) # should be \x00\x06\x34\x00\x00\x02\x00\x00
+        print 'recv 1',data.encode('hex')
+        print 'expected  00 06 34 00 00 02 00 00'
+        conn.send('\x00\x6a\x34\x10\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        data = conn.recv(1024) # seems to be 542
+        print 'recv 2', data.encode('hex')
+        print 'expected 542, got ',str(len(data))
+        conn.send('\x00\x0a\x34\x11\x00\x01\x00\x00\xff\xff\xff\xff')
+        data = conn.recv(1024) # 6 bytes? or 86 bytes? first difference between connections
+        print 'recv 3b', data.encode('hex')
+        if data == '\x00\x00': # for some large files, suddenly the null-bytes appear here.. what the hell..?
+            data = conn.recv(1024) # 6 bytes? or 86 bytes? first difference between connections
+            gotnullbytes = True
+            print 'recv 3b-afternull', data.encode('hex')
 
-    if data == '\x00\x04\x36\x01\xFF\xF1':
-        # this was apparently an 'are you there?'-call
-        conn.close()
-        continue
+        if data == '\x00\x04\x36\x01\xFF\xF1':
+            # this was apparently an 'are you there?'-call
+            conn.close()
+            continue
 
-    # data = conn.recv(1024) # 86 bytes. this is the first difference in the second connectionstream
-    filename = data[38:62].strip('\x00') # byte 37 is $, then 24 bytes of filename follow
-    print 'recv 3', len(data), data
-    f = open(os.path.join(PATH, filename),'w')
-    conn.recv(2) # two initial nullbytes
-    remainingblock = 0
-    print 'Reading file..'
-    while True:
-        data = conn.recv(1024)
-        if data == '\x00\x04\x30\x02\x00\x00':
-            print 'found closing string..'
-            break
-        if len(data) == 0:
-            # raise Exception("Found zero data string.")
-            print "Zero data string found"
-            break
-        if remainingblock == 0:
-            remainingblock = data[:2]
-            data = data[2:]
-            remainingblock = int(remainingblock.encode('hex'), 16)
-        if len(data) > remainingblock:
-            raise Exception("Assumed that the protocol did not split packets.")
+        filename = os.path.join(PATH, data[38:62].strip('\x00')) # byte 37 is $, then 24 bytes of filename follow
+        print filename
+        print 'recv 3', len(data), data
+        f = open(filename,'w')
+        # debug = open('debug','w')
+        data = conn.recv(2) # maybe two initial nullbytes
+        print 'recv 3c', data.encode('hex')
+        retaindata = False
+        if data == '\x00\x00':
+            if gotnullbytes:
+                print "Two blocks of 2 nullbytes!"
+            else:
+                print 'Confirming nullbytes'
         else:
-            remainingblock -= len(data)
-            f.write(data)
-        # print 'Packet received:',len(data)
-    f.close()
-    print 'Saved file'
-    # sends another package after the closing string, before expecting a reply 
-    data = conn.recv(6)
-    print 'recv 4' # 00 04 30 05 bf d5 discarded
-    conn.send('\x00\x06\x30\x15\x00\x01\x00\x00')
-    print 'sent 4'
-    data = conn.recv(6) # 00 04 30 03 00 00
-    print 'recv 5', data.encode('hex')
-    conn.send('\x00\x06\x30\x13\x00\x01\x00\x00')
-    print 'sent 5'
-    conn.close()
-    print 'done'
+            print 'Oops. Ate initial bytes off the PDF while zero-testing'
+            retaindata = True
+        remainingblock = 0
+        print 'Reading file..'
+        canceled = False
+        while True:
+            if retaindata:
+                print 'Retaining data..'
+                # data += conn.recv(1024)
+                retaindata = False
+            else:
+                data = conn.recv(1024)
+            # debug.write(data) # starts with 0a69?
+            if len(data) < 100:
+                print 'Data trace:'
+                print data.encode('hex')
+            if data == '\x00\x04\x30\x02\x00\x00':
+                print 'found closing string..'
+                break
+            if len(data) == 0:
+                print "User seems to have pressed cancel during scan.."
+                canceled = True
+                break
+            if remainingblock == 0:
+                datahead = data
+                remainingblock = data[:2]
+                data = data[2:]
+                remainingblock = int(remainingblock.encode('hex'), 16)
+            if len(data) > remainingblock:
+                print 'This should not be happening', len(data), remainingblock
+                print 'Data head:',datahead.encode('hex')
+                print 'data at the moment:', data.encode('hex')
+                f.write(data[:remainingblock])
+                print 'Written to file:', data[:remainingblock].encode('hex')
+                data = data[remainingblock:]
+                remainingblock = 0
+                print 'data at the moment:', data.encode('hex')
+                retaindata = True
+            else:
+                remainingblock -= len(data)
+                f.write(data)
+        # debug.close()
+        f.close()
+        if canceled:
+            os.remove(filename)
+            print 'Removed file'
+            conn.close()
+            continue
+        print 'Saved file'
+        # sends another package after the closing string, before expecting a reply 
+        data = conn.recv(6)
+        print 'recv 4' # 00 04 30 05 bf d5 discarded
+        # capture of 1 long file: 00 04 30 05 36 ce
+        conn.send('\x00\x06\x30\x15\x00\x01\x00\x00')
+        data = conn.recv(6) # 00 04 30 03 00 00
+        print 'recv 5', data.encode('hex')
+        conn.send('\x00\x06\x30\x13\x00\x01\x00\x00')
+        print 'done'
+    except Exception as inst:
+        print inst
+    finally:
+        conn.close()
+        
